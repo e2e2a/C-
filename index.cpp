ff7ecc08a80629934efd5b593b4a2cff6c1eb17c
e2e2a <emonawong22@gmail.com>
2024-04-12 17:40:25 +0800

    Add

---
+++ index.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string log1 = "hello1 World123";
+  int m = 1;
+  string log2 = "Me";
+  cout << log1 << " " << m << " " << log2;
+  return 0;
+}
