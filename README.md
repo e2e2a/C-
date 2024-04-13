7626f6fb5784abc07f73cfe96261672973fb479c
e2e2a <emonawong22@gmail.com>
2024-04-13 14:22:09 +0800

    add

---
+++ README.md
@@ -0,0 +1,16 @@
+# C++ 
+This repository contains beginner-level C++ programs.
+
+## Building
+
+Run the command below to build
+```
+g++ -o index.exe index.cpp
+```
+this will create index.exe
+
+## Run
+Run the command below to run index.exe
+```
+./index
+```
--- index.cpp
+++ index.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
   string log1 = "hello1 World123";
   int m = 1;
+  bool b = true;
   string log2 = "Me";
-  cout << log1 << " " << m << " " << log2;
+  string log3 = "bool is false";
+  if (b)
+  {
+    cout << log1 << " " << m << " " << log2;
+  }
+  else
+  {
+    cout << log3;
+  }
   return 0;
 }
